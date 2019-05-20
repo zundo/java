106a667040c764156c9788e787b0520f805d7cce
zundo <45172636+zundo@users.noreply.github.com>
2019-05-20 14:21:53 +0200

    jeu de l'oie

---
+++ Main.java
@@ -0,0 +1,37 @@
+import java.util.Random;
+import java.util.Scanner;
+
+import javax.swing.Popup;
+
+public class Main {
+	
+	public static void main (String[] args){
+		
+		/*Random rd = new Random();
+		//System.out.println("random="+rd.nextInt(100));
+        Scanner sc = new Scanner(System.in); 
+        // Initier le scanner, System in = une entrée clavier
+        int leprix = 0;
+        int random = rd.nextInt(100);
+        int count = 0;
+        //System.out.println("random"+random);
+        while(leprix != random) {
+        	System.out.println("Entrez un chiffre : ");
+            count++;
+        	leprix = sc.nextInt();
+		        if (leprix > random){
+		        	System.out.println("C'est moins");
+		        }
+		        if (leprix < 
+		        		random){
+		        	System.out.println("C'est plus");
+		        }
+		    
+        }*/
+        //System.out.println("Bravo le juste prix est "+random);
+        //System.out.println("Vous avez gagner en  "+count +" tours");
+		
+		fenetre toto = new fenetre();
+        
+	}
+}
---
+++ fenetre.java
@@ -0,0 +1,68 @@
+
+import java.awt.Color;
+import java.awt.Container;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Random;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class fenetre {
+	JFrame frame = new JFrame();
+	int count = 0;
+	private Container panel;
+	public fenetre() {
+		JLabel label = new JLabel("Le Juste prix");
+		frame.add(label);
+		label.setHorizontalAlignment(JLabel.CENTER);
+		frame.setLayout(new GridLayout(3,1));
+		JButton button = new JButton("envoyer");
+
+		JTextField text = new JTextField("Text");
+		text.setHorizontalAlignment(JTextField.CENTER);
+		Random rd = new Random();
+		int random = rd.nextInt(100);
+		button.setBackground(Color.CYAN);
+		
+		button.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				
+				int leprix = 0;
+		        
+		        
+		        leprix = Integer.valueOf(text.getText());
+		        String text = "";
+		        if (leprix > random){
+		        		text = "moins";
+		        } 
+		        if (leprix < random)
+		        {
+		        	text = "C'est plus";
+		        }
+		        count++;
+		        if (leprix == random){
+		        //System.out.println("Bravo le juste prix est "+random);
+		        //System.out.println("Vous avez gagner en  "+count +" tours");
+		        text = "Vous avez gagner en "+count + " tours, le juste prix est "+random;
+		        }
+		        JOptionPane.showMessageDialog(null, text);
+			}
+		
+		});
+		
+		frame.add(text);
+		frame.add(button);
+		frame.pack();
+		frame.setVisible(true);	
+		
+	
+	}
+}
